ce150b1eb0ee116850bafd7baf19c2709417f774
kayemafnan <kayemmd23@gmail.com>
2026-01-05 01:30:51 +0600

    Add files via upload

---
+++ Find the smallest integer in the array.dart
@@ -0,0 +1,14 @@
+//Given an array of integers your solution 
+//should find the smallest integer.
+// For example:
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return
+//----------sulotion-----------------
+void main(){
+  
+}
+
+int findSmallestInt(List<int> arr) {
+  // Code here
+  return 0;
+}
